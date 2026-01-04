26fb813ef40e708790f2134e99840b2b3e988d70
Sabbir Ahmed Jisan <jisancse875@gmail.com>
2026-01-05 01:51:49 +0600

    Refactor Tenant class to include password hashing

--- TenantManagement/models/tenant.py
+++ TenantManagement/models/tenant.py
@@ -1,51 +1,63 @@
-from models.user import User
+import hashlib
 from datetime import datetime
-
-
-class Tenant(User):
-    def __init__(self, tenant_id, name, phone, username, password):
-        super().__init__(username, password, role="tenant")
+from models.payment import Payment
+class Tenant:
+    def __init__(self, tenant_id, name, phone, username, password_hash, role="tenant", assigned_flat_id=None,
+                 flat_rent=0.0, payments=None, is_hashed=True):
         self.tenant_id = tenant_id
         self.name = name
         self.phone = phone
-        self.assigned_flat_id = None
-        self.flat_rent = 0.0
-        self.payments = []
-
-    def assign_flat(self, flat_id, rent):
-        self.assigned_flat_id = flat_id
-        self.flat_rent = float(rent)
-
-    def add_payment(self, payment_obj):
-        self.payments.append(payment_obj)
-
-    def get_pending_dues(self):
-        if not self.assigned_flat_id:
-            return 0.0
-
-        current_month = datetime.now().strftime("%B")
-        is_paid = any(p.month.lower() == current_month.lower() for p in self.payments)
-
-        if is_paid:
-            return 0.0
+        self.username = username
+        if is_hashed:
+            self.password_hash = password_hash
         else:
-            return self.flat_rent
+            self.password_hash = hashlib.sha256(password_hash.encode()).hexdigest()
+        self.role = role
+        self.assigned_flat_id = assigned_flat_id
+        self.flat_rent = float(flat_rent)
+        self.payments = payments if payments else []
 
+    def verify_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest() == self.password_hash
+
+    def get_due_amount(self):
+        if not self.assigned_flat_id or self.flat_rent == 0:
+            return 0.0
+        current_month = datetime.now().strftime("%B")
+
+        for p in reversed(self.payments):
+            if p.month == current_month:
+                return 0.0
+        return self.flat_rent
+
+    def add_payment(self, amount, month):
+        p = Payment(amount, month, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        self.payments.append(p)
     def to_dict(self):
-        data = super().to_dict()
-        data.update({
+        return {
+            "username": self.username,
+            "password_hash": self.password_hash,
+            "role": self.role,
             "tenant_id": self.tenant_id,
             "name": self.name,
             "phone": self.phone,
             "assigned_flat_id": self.assigned_flat_id,
             "flat_rent": self.flat_rent,
             "payments": [p.to_dict() for p in self.payments]
-        })
-        return data
-
+        }
     @classmethod
     def from_dict(cls, data):
-        t = cls(data['tenant_id'], data['name'], data['phone'], data['username'], data['password_hash'])
-        t.assigned_flat_id = data.get('assigned_flat_id')
-        t.flat_rent = data.get('flat_rent', 0.0)
-        return t
+        payments = [Payment.from_dict(p) for p in data.get("payments", [])]
+        def_hash = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"
+        return cls(
+            tenant_id=data.get("tenant_id"),
+            name=data.get("name"),
+            phone=data.get("phone"),
+            username=data.get("username"),
+            password_hash=data.get("password_hash", def_hash),
+            role=data.get("role", "tenant"),
+            assigned_flat_id=data.get("assigned_flat_id"),
+            flat_rent=data.get("flat_rent", 0.0),
+            payments=payments,
+            is_hashed=True
+        )
